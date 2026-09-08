4ad98812339cead9c13c122dda14d60cbf9cb08f
GabrielGL11 <gabrielguaman1105@gmail.com>
2026-09-08 14:35:26 -0500

    feat: add course read endpoints

--- src/welcome.service.ts
+++ src/welcome.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';//1
 @Injectable()//2
 export class WelcomeService {//3
     getMessage(): {message: string} {//4
-        return {message: 'Bienvenido a CourseHub API'};//5
+        return {message: 'Bienvenido a CourseHub API' };//5
     }   
 }   
